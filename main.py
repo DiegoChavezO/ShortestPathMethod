c5867868d8d933bba18d7c22f2b142509c148484
LioGhost <goliat282@gmail.com>
2023-06-11 20:55:18 -0600

    Actualizacion de variables y titulos

--- main.py
+++ main.py
@@ -3,80 +3,79 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 class Graph:
-    def __init__(self, graph):
-        self.graph = graph
-        self.rows = len(graph)
+    def __init__(self, grafo):
+        self.grafo = grafo
+        self.nodos = len(grafo)
 
-    def bfs(self, s, t, parent):
-        visited = [False] * self.rows
+    def bfs(self, s, t, padre):
+        visitado = [False] * self.nodos
         queue = []
         queue.append(s)
-        visited[s] = True
+        visitado[s] = True
 
         while queue:
             u = queue.pop(0)
-            for ind, val in enumerate(self.graph[u]):
-                if not visited[ind] and val > 0:
+            for ind, val in enumerate(self.grafo[u]):
+                if not visitado[ind] and val > 0:
                     queue.append(ind)
-                    visited[ind] = True
-                    parent[ind] = u
+                    visitado[ind] = True
+                    padre[ind] = u
                     if ind == t:
                         return True
 
         return False
 
-    def ford_fulkerson(self, source, sink):
-        parent = [-1] * self.rows
-        max_flow = 0
+    def ford_fulkerson(self, inicio, destino):
+        padre = [-1] * self.nodos
+        max_flujo = 0
 
-        while self.bfs(source, sink, parent):
-            path_flow = float("Inf")
-            s = sink
-            while s != source:
-                path_flow = min(path_flow, self.graph[parent[s]][s])
-                s = parent[s]
+        while self.bfs(inicio, destino, padre):
+            ruta_flujo = float("Inf")
+            s = destino
+            while s != inicio:
+                ruta_flujo = min(ruta_flujo, self.grafo[padre[s]][s])
+                s = padre[s]
 
-            max_flow += path_flow
-            v = sink
-            while v != source:
-                u = parent[v]
-                self.graph[u][v] -= path_flow
-                self.graph[v][u] += path_flow
-                v = parent[v]
+            max_flujo += ruta_flujo
+            v = destino
+            while v != inicio:
+                u = padre[v]
+                self.grafo[u][v] -= ruta_flujo
+                self.grafo[v][u] += ruta_flujo
+                v = padre[v]
 
-        return max_flow
+        return max_flujo
 
     def display_graph(self):
-        print("Final Residual Graph:")
-        for i in range(self.rows):
-            for j in range(self.rows):
-                print(self.graph[i][j], end=" ")
+        print("Grafo Residual Final:")
+        for i in range(self.nodos):
+            for j in range(self.nodos):
+                print(self.grafo[i][j], end=" ")
             print()
 
         G = nx.DiGraph()
-        for i in range(self.rows):
-            for j in range(self.rows):
-                if self.graph[i][j] > 0:
-                    G.add_edge(i, j, capacity=self.graph[i][j])
+        for i in range(self.nodos):
+            for j in range(self.nodos):
+                if self.grafo[i][j] > 0:
+                    G.add_edge(i, j, capacity=self.grafo[i][j])
 
         pos = nx.spring_layout(G)
-        labels = nx.get_edge_attributes(G, 'capacity')
+        labels = nx.get_edge_attributes(G, 'capacidad')
         nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500)
         nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-        plt.title("Residual Graph")
+        plt.title("Grafo Residual")
         plt.show()
 
-# User input for the graph
-graph = []
-vertices = int(input("Enter the number of vertices: "))
+grafo = []
+vertices = int(input("Ingrese el numero de vertices: "))
 for i in range(vertices):
-    row = list(map(int, input(f"Enter the capacities for edges from vertex {i}: ").split()))
-    graph.append(row)
+    row = list(map(int, input(f"Ingrese las capacidades de los bordes del nodo {i + 1}: ").split()))
+    grafo.append(row)
 
-source = int(input("Enter the source vertex: "))
-sink = int(input("Enter the sink vertex: "))
+inicio = int(input("Ingrese el nodo inicial: ")) - 1
+destino = int(input("Ingrese el nodo destino: ")) - 1
 
-g = Graph(graph)
-max_flow = g.ford_fulkerson(source, sink)
-print("The maximum flow in the graph is:", max_flow)
+g = Graph(grafo)
+max_flujo = g.ford_fulkerson(inicio, destino)
+print("El máximo flujo en el grafo es:", max_flujo)
 g.display_graph()
